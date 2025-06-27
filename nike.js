eaee339a3b36d92b049ee8ed37bcfd4fd6b85f96
NaveenPundir123 <naveensinghpundir123@gmail.com>
2025-06-27 16:47:39 +0530

    shop by icon pending

---
+++ nike.js
@@ -0,0 +1,7 @@
+function scrollLeft() {
+  document.getElementById("scrollContainer").scrollLeft -= 500;
+}
+
+function scrollRight() {
+  document.getElementById("scrollContainer").scrollLeft += 400;
+}
